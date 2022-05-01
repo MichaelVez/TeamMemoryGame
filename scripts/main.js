47c340bb57604c00e48cc7370f413476bed1d704
MichaelVez <michaelx979@gmail.com>
2022-05-01 18:43:39 +0300

    card logic skeleton added

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,5 @@ import { start_crateLayout as crateBored } from "./start_cratelayout.js";
 crateBored(gridsizex, gridsizey); //param height and width
 import { start_random } from "./start_random.js";
 start_random(gridsizex * gridsizey);
+import { eventListenersCards } from "./event-listeners-cards.js";
+eventListenersCards();
