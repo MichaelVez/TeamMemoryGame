418b97e4d8b289069723b718364305434b4d0853
Ofir-Cohen-1 <ofir.cohen22@gmail.com>
2022-05-02 18:30:08 +0300

    timer2

--- scripts/events-btns.js
+++ scripts/events-btns.js
@@ -2,22 +2,35 @@
 //startTimer()
 //stopTimer()
 
-export function startTimer() {
-  const timerCount = document.querySelector(".timer .count");
+const timerCount = document.querySelector(".timer .count");
+const btnStart = document.querySelector("#btnStart");
+const btnStop = document.querySelector("#btnStop");
+let check = null;
+
+function startTimer() {
   let seconds = 0;
   let minuts = 0;
-  setInterval(() => {
+  check = setInterval(() => {
     let total = minuts < 10 ? "0" + minuts : minuts;
     seconds++;
     timerCount.innerText =
       seconds < 10 ? total + ":0" + seconds : total + ":" + seconds;
-    if (seconds === 60) {
-      seconds = 0;
+    if (seconds === 59) {
+      seconds = -1;
       minuts++;
     }
   }, 1000);
 }
 
-export function stopTImer() {
-  clearInterval(startTimer());
-};
+btnStart.addEventListener("click", () => {
+  startTimer();
+});
+
+function stopTimer() {
+  clearInterval(check);
+  check = null;
+}
+
+btnStop.addEventListener("click", () => {
+  stopTimer();
+});
