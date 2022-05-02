563c58c1f6fed88043ac25611db48a9308c87d0b
Ofir-Cohen-1 <ofir.cohen22@gmail.com>
2022-05-02 13:27:57 +0300

    first commit

--- scripts/events-btns.js
+++ scripts/events-btns.js
@@ -2,5 +2,22 @@
 //startTimer()
 //stopTimer()
 
-export function startTimer() {}
-export function stopTImer() {}
+export function startTimer() {
+  const timerCount = document.querySelector(".timer .count");
+  let seconds = 0;
+  let minuts = 0;
+  setInterval(() => {
+    let total = minuts < 10 ? "0" + minuts : minuts;
+    seconds++;
+    timerCount.innerText =
+      seconds < 10 ? total + ":0" + seconds : total + ":" + seconds;
+    if (seconds === 60) {
+      seconds = 0;
+      minuts++;
+    }
+  }, 1000);
+}
+
+export function stopTImer() {
+  clearInterval(startTimer());
+};
